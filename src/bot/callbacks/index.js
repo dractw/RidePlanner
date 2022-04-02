7f3f239a563ea532b2062ee4693538eb6a7beb49
dractw <dractw@gmail.com>
2022-04-02 19:11:55 +0300

    [ref] const

--- src/bot/callbacks/index.js
+++ src/bot/callbacks/index.js
@@ -1,18 +1,18 @@
 const { Markup } = require('telegraf')
-const { getAllRides, createUser } = require('../../handlers')
+const { Keyboard_buttons } = require('../../const')
+const { getAllRides } = require('../../handlers')
 
 const main_menu = (ctx) => {
-  ctx.reply('@ride_planner_bot', Markup.inlineKeyboard([
-    Markup.button.callback('Ближайшие прохваты', 'show_upcoming_rides'),
+  ctx.reply('@ride_planner_bot', Markup.keyboard([
+    Markup.button.callback(Keyboard_buttons.SHOW_UPCOMING),
 
-    Markup.button.callback('Запланировать новый', 'create_new_ride'),
+    Markup.button.callback(Keyboard_buttons.CREATE_NEW_RIDE),
   ]))
 }
 
 const show_upcoming_rides = (ctx) => {
   getAllRides()
     .then((rides) => {
-      console.log(rides)
       return ctx.reply(JSON.stringify(rides))
     })
     .catch((e) => {
@@ -21,17 +21,7 @@ const show_upcoming_rides = (ctx) => {
 }
 
 const create_new_ride = async (ctx) => {
-  const { id, last_name, first_name, username } = ctx.update.callback_query.from
-
-  const user_params = {
-    user_id: id,
-    name: `${first_name} ${last_name}`,
-    username,
-  }
-
-  createUser(user_params)
-
-  ctx.reply('create new')
+  ctx.scene.enter('CREATE_RIDE_WIZARD')
 }
 
 module.exports = {
---
+++ src/const/index.js
@@ -0,0 +1,8 @@
+const Keyboard_buttons = {
+  CREATE_NEW_RIDE: '🗺 Запланировать новый',
+  SHOW_UPCOMING: '🏍 Ближайшие прохваты',
+}
+
+module.exports = {
+  Keyboard_buttons,
+}
