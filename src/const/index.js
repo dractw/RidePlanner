cad4b6ef92aa7d16d2621cc7cf72283a1c1bbfbb
dractw <dractw@gmail.com>
2022-04-02 21:02:59 +0300

    [const] Keyboard_buttons

--- src/const/index.js
+++ src/const/index.js
@@ -1,8 +1,35 @@
 const Keyboard_buttons = {
-  CREATE_NEW_RIDE: '🗺 Запланировать новый',
-  SHOW_UPCOMING: '🏍 Ближайшие прохваты',
+  CREATE_NEW_RIDE: {
+    title: '🗺 Запланировать новый',
+  },
+  SHOW_UPCOMING: {
+    title: '🏍 Ближайшие прохваты',
+  },
+  CANCEL: {
+    title: '❌ Отменить',
+    cb: 'cancel_scene',
+  },
+  BACK: {
+    title: '🔙 Назад',
+    cb: 'step_back',
+  },
+  HARD: {
+    title: '🌶🌶🌶',
+    cb: 'level_hard',
+  },
+  MID: {
+    title: '🌶🌶',
+    cb: 'level_mid',
+  },
+  EASY: {
+    title: '🌶',
+    cb: 'level_easy',
+  },
 }
+
+const LEVELS = [Keyboard_buttons.EASY.cb, Keyboard_buttons.HARD.cb, Keyboard_buttons.MID.cb]
 
 module.exports = {
   Keyboard_buttons,
+  LEVELS,
 }
