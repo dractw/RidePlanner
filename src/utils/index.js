fff5eb8a18e95abfdf3278058c48574275a04cbc
dractw <dractw@gmail.com>
2022-05-16 23:47:28 +0300

    [utils] feat: bot name depends on env `prod` or `dev`

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,18 @@ const moment = require('moment')
 const marked = require('marked')
 const { levels_cb } = require('../const')
 
-const get_ride_markdown = (ride, author) => {
+const get_bot_name = () => {
+  const { NODE_ENV } = process.env
+
+  switch (NODE_ENV) {
+    case 'production':
+      return '@ride_planner_bot'
+    case 'dev':
+      return '@ride_planner_dev_bot'
+  }
+}
+
+const get_ride_markdown = (ride, author, external_message) => {
   const { title, date, start_point, level, participants, description, _id } = ride
   const markdown_obj = {
     title: `**${title.toString().toUpperCase()}** (\`${_id}\`)`,
@@ -14,13 +25,14 @@ const get_ride_markdown = (ride, author) => {
     author: `Орг: [${author.username}](tg://user?id=${author.user_id})`,
   }
 
-  return marked.parseInline(Object.keys(markdown_obj).reduce((acc, next) => acc + markdown_obj[next] + '\n', ''))
+  return marked.parseInline(Object.keys(markdown_obj).reduce((acc, next) => acc + markdown_obj[next] + '\n', external_message ? `${external_message}\n\n` : ''))
 }
 
-const default_bot_reply = async (ctx) => await ctx.reply('@ride_planner_bot - бот для отслеживания запланированых прохватов и событий\n Напиши ему в личные сообщения, он тебе всё расскажет ;)')
+const default_bot_reply = async (ctx) => await ctx.reply(`${get_bot_name()} - бот для отслеживания запланированых прохватов и событий\nНапиши ему в личные сообщения, он тебе всё расскажет ;)`)
   .catch((e) => console.error('Something  bad happens:', e))
 
 module.exports = {
+  get_bot_name,
   get_ride_markdown,
   default_bot_reply,
 }
