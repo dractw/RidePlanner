368cae50dabc817f1ac0b0930df3a9dae89fcebe
dractw <dractw@gmail.com>
2022-05-18 20:01:13 +0300

    [const] welcome_message.js

---
+++ src/const/welcome_message.js
@@ -0,0 +1,10 @@
+const defined_chats = {
+  '-1001281360655': {
+    link: 'https://t.me/funendurospb/3',
+  },
+  '-644132853': {
+    link: 'https://t.me/funendurospb/3',
+  },
+}
+
+module.exports = defined_chats
